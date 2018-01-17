1e5382d2c232042bf2de2f42302a173d16571b26
RaspberryPi <petrahesselmar@gmail.com>
2018-01-17 20:10:58 +0000

    Added .java

---
+++ helloworld.java
@@ -0,0 +1,9 @@
+/**
+*TEST!! The helloworld class implements an application that simply print "Hello World!"
+*/
+
+class helloworld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!"); //Display the string.
+	}
+}
